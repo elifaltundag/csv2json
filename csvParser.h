8723cf98869e35403cfc821c1cb3bda2a761f99a
elifaltundag <elif.altundag.x@gmail.com>
2024-05-05 16:18:16 +0300

    Remove commented out previous versions of function declarations (without pParams, with separate arguments)

--- csvParser.h
+++ csvParser.h
@@ -4,26 +4,11 @@
 #include "IntArr.h"
 
 // Written
-// void determineNumCols(FILE* pfInput, char* pDelimiter, int* pNumCols);
 void determineNumCols(Parameters* pParams);
-
-
 int bufferHeaders(FILE* pfInput, CharArr* pArrBufferHeader);
-
-
-// void countLines(FILE* pfInput, char* pDelimiter, int* pNumValueLines, int* pNumCommentLines, bool* pHasHeaders);
 void countLines(Parameters* pParams);
-
-
-// int eachLineHasSameNumCols(FILE* pfInput, char* pDelimiter, const int* pNumCols);
 int eachLineHasSameNumCols(Parameters* pParams);
 
-
 // Will be improved for edge cases
-
-// int parseCSV(FILE* pfInput, char* pDelimiter, int* pNumCols, int* pNumValueLines, int* pNumCommentLines, bool* pHasHeaders, char*** pppHeaderList);
 int parseCSV(Parameters* pParams);
-
-
-// int validateData(FILE* pfInput, char* pDelimiter, const int* pNumCols, int* pNumValueLines, int* pNumCommentLines, bool* pHasHeaders);
 int validateData(Parameters* pParams);
